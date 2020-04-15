4255044928c16d777436ed76f4fd3bd25b8a71f5
Androz2091 <androz2091@gmail.com>
2020-04-15 15:12:57 +0200

    :sparkles: Add message guild raw and message channel raw

---
+++ src/blocks/discord/message/message_channel_raw.js
@@ -0,0 +1,55 @@
+import Blockly from "blockly/core";
+import { registerRestrictions } from "../../../restrictions";
+
+const blockName = "s4d_message_channel_raw";
+
+const blockData = {
+    "message0": "%{BKY_MESSAGE_CHANNEL_RAW}",
+    "args0": [
+        {
+            "type": "field_dropdown",
+            "name": "SEARCH_TYPE",
+            "options": [
+                [
+                    "%{BKY_NAME}",
+                    "NAME"
+                ],
+                [
+                    "id",
+                    "ID"
+                ]
+            ]
+        }
+    ],
+    "output": "String",
+    "colour": "#9966FF",
+    "tooltip": "",
+    "helpUrl": ""
+};
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function(block) {
+    const searchType = block.getFieldValue("SEARCH_TYPE");
+    if(searchType === "ID"){
+        const code = ["(s4d.message.channel || {}).id", Blockly.JavaScript.ORDER_NONE];
+        return code;
+    } else if(searchType === "NAME"){
+        const code = ["(s4d.message.channel || {}).name", Blockly.JavaScript.ORDER_NONE];
+        return code;
+    }
+};
+
+registerRestrictions(blockName, [
+    {
+        type: "toplevelparent",
+        message: "RES_MUST_BE_IN_ON_MESSAGE",
+        types: [
+            "s4d_on_message"
+        ]
+    }
+]);
--- src/blocks/discord/message/message_guild.js
+++ src/blocks/discord/message/message_guild.js
@@ -1,28 +1,12 @@
-import * as Blockly from "blockly";
+import Blockly from "blockly/core";
 import { registerRestrictions } from "../../../restrictions";
 
 const blockName = "s4d_message_guild";
 
 const blockData = {
     "message0": "%{BKY_MESSAGE_GUILD}",
-    "args0": [
-        {
-            "type": "field_dropdown",
-            "name": "SEARCH_TYPE",
-            "options": [
-                [
-                    "%{BKY_NAME}",
-                    "NAME"
-                ],
-                [
-                    "id",
-                    "ID"
-                ]
-            ]
-        }
-    ],
-    "output": "String",
     "colour": "#9966FF",
+    "output": "Server",
     "tooltip": "",
     "helpUrl": ""
 };
@@ -33,15 +17,9 @@ Blockly.Blocks[blockName] = {
     }
 };
 
-Blockly.JavaScript[blockName] = function(block) {
-    const searchType = block.getFieldValue("SEARCH_TYPE");
-    if(searchType === "ID"){
-        const code = ["(s4d.message.guild || {}).id", Blockly.JavaScript.ORDER_NONE];
-        return code;
-    } else if(searchType === "NAME"){
-        const code = ["(s4d.message.guild || {}).name", Blockly.JavaScript.ORDER_NONE];
-        return code;
-    }
+Blockly.JavaScript[blockName] = function() {
+    const code = ["s4d.message.guild", Blockly.JavaScript.ORDER_NONE];
+    return code;
 };
 
 registerRestrictions(blockName, [
