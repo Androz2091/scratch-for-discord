9daf37ead41b1ac0d9c9c42ec88114c81c2eb0fb
Androz2091 <androz2091@gmail.com>
2020-04-18 08:59:57 +0200

    :zap: Only accept s4d files

--- src/components/NavigationBar/FileMenu.vue
+++ src/components/NavigationBar/FileMenu.vue
@@ -1,7 +1,7 @@
 <template>
      <b-nav-item-dropdown :text="$t('file.title')" right>
         <b-dropdown-item @click="askForFile">{{ $t('file.open') }}</b-dropdown-item>
-        <input hidden @change="load" id="load-code" type="file">
+        <input hidden @change="load" id="load-code" type="file" accept=".s4d">
         <b-dropdown-item v-b-modal.code-modal>{{ $t('file.javascript') }}</b-dropdown-item>
         <b-dropdown-item @click="save">{{ $t('file.save') }}</b-dropdown-item>
     </b-nav-item-dropdown>
