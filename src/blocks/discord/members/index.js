058c7e72b76c268c2dd6ac2e5d43eea4c86cfec8
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-04-18 14:32:08 +0200

    Update index.js

--- src/blocks/discord/members/index.js
+++ src/blocks/discord/members/index.js
@@ -1,4 +1,8 @@
 import "./ban_member";
 import "./get_member";
 import "./kick_member";
+import "./member_discriminator";
+import "./member_id";
+import "./member_tag"
+import "./member_username"
 import "./set_member_nickname";
