2a73df981c09a733957aa2d2cd8330e7fe1f86eb
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-04-18 14:23:29 +0200

    Create member_discriminator.js

---
+++ src/blocks/discord/members/member_discriminator.js
@@ -0,0 +1,42 @@
+import Blockly from "blockly/core";
+import { registerRestrictions } from "../../../restrictions";
+
+const blockName = "s4d_member_discriminator
+
+const blockData = {
+    "message0": "%{BKY_MEMBER_DISCRIMINATOR}"
+    "args0": [
+        {
+            "type": "input_value",
+            "name": "MEMBER",
+            "check": "Member"
+        }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#4C97FF",
+    "tooltip": "",
+    "helpUrl": ""
+};
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function(block) {
+    const member = Blockly.JavaScript.valueToCode(block, "MEMBER", Blockly.JavaScript.ORDER_ATOMIC);
+    const code = `${member}.user.discriminator;\n`;
+    return code;
+};
+
+registerRestrictions(blockName, [
+    {
+        type: "notempty",
+        message: "RES_MEMBER_DISCRIMINATOR_MISSING_MEMBER",
+        types: [
+            "MEMBER"
+        ]
+    }
+]);
