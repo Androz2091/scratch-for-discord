0c79f4256c415f3822bfbdf458e64823a51b18f1
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-04-18 13:20:36 +0200

    Update index.js

--- src/blocks/discord/servers/index.js
+++ src/blocks/discord/servers/index.js
@@ -1,6 +1,7 @@
-import "./boost_count.js";
+import "./boost_count";
 import "./get_server";
 import "./icon_url";
 import "./member_count";
 import "./server_name";
+import "./server_owner";
 import "./set_server_name";
