b86ac7a7421a0c7e8571f1fb9da15f08d17efb82
Androz2091 <androz2091@gmail.com>
2020-04-21 12:13:47 +0200

    :truck: Move member has permission block in Members category

--- src/blocks/discord/members/index.js
+++ src/blocks/discord/members/index.js
@@ -2,6 +2,7 @@ import "./ban_member";
 import "./get_member";
 import "./kick_member";
 import "./member_discriminator";
+import "./member_has_permission";
 import "./member_id";
 import "./member_tag";
 import "./member_username";
