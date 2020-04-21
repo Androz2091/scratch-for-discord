615520dfba0d149c11eebf06b1fb7a610ba0926f
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-04-21 15:59:04 +0400

    Create member_is_bot.js (#19)
    * Create member_is_bot.js
    
    * Update toolbox.js
    
    * Update fr-FR.json
    
    * Update en-US.json
    
    * Update fr-FR.json
    
    * Update en-US.json
    
    * Update index.js
    
    * Update toolbox.js
    
    * Update member_is_bot.js
    
    * Update member_is_bot.js

--- src/blocks/discord/members/index.js
+++ src/blocks/discord/members/index.js
@@ -4,6 +4,7 @@ import "./kick_member";
 import "./member_discriminator";
 import "./member_has_permission";
 import "./member_id";
+import "./member_is_bot";
 import "./member_tag";
 import "./member_username";
 import "./set_member_nickname";
