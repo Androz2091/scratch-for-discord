046be987a363309ca3af3be4f91f8751c04f75af
Androz2091 <androz2091@gmail.com>
2020-04-25 11:31:20 +0200

    :sparkles: Add wait seconds block

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,7 @@ import "./blocks/discord/members/";
 import "./blocks/discord/reactions/";
 import "./blocks/database/";
 import "./blocks/text/";
+import "./blocks/loops/";
 import "./prompt";
 
 export default {
