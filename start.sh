1c0db0416bc8465818bdb9974388321dfa794025
Androz2091 <androz2091@gmail.com>
2020-04-25 12:49:07 +0200

    :fire: Remove start.sh file

--- start.sh
+++
@@ -1,8 +0,0 @@
-if [ "$(npm list -g | grep yarn)" ]
-then
-    yarn list | grep discord.js || yarn add discord.js --no-lockfile
-    node bot.js
-else
-    npm list | grep discord.js || npm install discord.js --no-shrinkwrap
-    node bot.js
-fi
