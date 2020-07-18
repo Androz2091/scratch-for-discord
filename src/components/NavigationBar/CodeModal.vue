2e5e318b51a7dead1ddbc6a7c0b5667167581130
Androz2091 <androz2091@gmail.com>
2020-07-18 15:06:06 +0200

    :lipstick: Update export to javascript modal css

--- src/components/NavigationBar/CodeModal.vue
+++ src/components/NavigationBar/CodeModal.vue
@@ -21,11 +21,24 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 
 #code {
-    width: 100%;
-    max-width:100%;
+    min-width: 100%;
+    min-height: 100%;
+}
+
+#code-modal .modal-dialog {
+    max-width: 100%;
+    margin: 0;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100vh;
+    display: flex;
+    position: fixed;
+    z-index: 100000;
 }
 
 </style>
