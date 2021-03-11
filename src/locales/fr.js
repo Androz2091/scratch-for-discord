e668272c71bd94fb50c81ad1850ab46bcd742631
jose_trindade1 <80281198+josetrindade1@users.noreply.github.com>
2021-03-11 10:18:03 -0300

    Update fr.js

--- src/locales/fr.js
+++ src/locales/fr.js
@@ -43,7 +43,8 @@ export const websiteMessages = {
         loaded: "Exemple {example} importé avec succès !",
         ping_pong: "Commande ping-pong",
         command_parsing: "Traitement des commandes",
-        leveling: "Système d'XP et niveaux"
+        leveling: "Système d'XP et niveaux",
+        handler:"Traitement des commandes - Avancé"
     },
     download: {
         title: "Télécharger votre bot",
