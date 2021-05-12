0100ae7929f54133268478a55d8cea5c17f4f876
jose_trindade1 <trindadej403@gmail.com>
2021-05-12 18:45:25 -0300

    erros fixed again

--- src/blocks/discord/bot-event/on_added.js
+++ src/blocks/discord/bot-event/on_added.js
@@ -24,6 +24,6 @@ Blockly.Blocks[blockName] = {
 
 Blockly.JavaScript[blockName] = function(block) {
     const statements = Blockly.JavaScript.statementToCode(block, "STATEMENTS");
-    const code = `s4d.client.on('guildCreate', async (s4dguild) => {\n${statements}\n});\n`;
+    const code = `s4d.client.on('guildCreate', async (s4dguild) => {\n${statements}\n});\n `;
     return code;
 };
