a7598fe48396b5e24a2e575d1c27a495d12fb1d0
jose_trindade1 <trindadej403@gmail.com>
2021-05-12 22:44:54 -0300

    errors fixed

--- src/blocks/discord/base/index.js
+++ src/blocks/discord/base/index.js
@@ -3,4 +3,3 @@ import "./bot_server_count";
 import "./login";
 import "./on_connected";
 import "./set_bot_game";
-import "./set_bot_satuts";
--- src/blocks/discord/base/set_bot_game.js
+++ src/blocks/discord/base/set_bot_game.js
@@ -33,6 +33,28 @@ const blockData = {
                 ]
             ]
         },
+        {
+            "type": "field_dropdown",
+            "name": "OIFD",
+            "options": [
+                [
+                    "%{BKY_ONLINE}",
+                    "online"
+                ],
+                [
+                    "%{BKY_OFFLINE}",
+                    "offline"
+                ],
+                [
+                    "%{BKY_IDLE}",
+                    "idle"
+                ],
+                [
+                    "%{BKY_DND}",
+                    "dnd"
+                ]
+            ]
+        },
     ],
     "colour": "#4C97FF",
     "previousStatement": null,
@@ -50,10 +72,11 @@ Blockly.Blocks[blockName] = {
 Blockly.JavaScript[blockName] = function(block){
     const type = block.getFieldValue("TYPE");
     const game = Blockly.JavaScript.valueToCode(block, "GAME", Blockly.JavaScript.ORDER_ATOMIC);
-    const code = `s4d.client.user.setActivity(String(${game}),{status: '${type}'}); \n`;
+    const OIFD =  block.getFieldValue("OIFD");
+    const code = `s4d.client.user.setPresence({status: "${OIFD}",activity:{name:${game},type:"${type}"}});; \n`;
     return code;
 };
-
+ 
 registerRestrictions(blockName, [
     {
         type: "notempty",
