98ec35c33e96a2bd14fca91c8fff8dca52599268
Androz2091 <androz2091@gmail.com>
2021-05-13 14:08:44 +0200

    :bug: Some fixes

--- src/blocks/text/replace.js
+++ src/blocks/text/replace.js
@@ -37,8 +37,8 @@ Blockly.Blocks[blockName] = {
 Blockly.JavaScript[blockName] = function(block) {
     const text = Blockly.JavaScript.valueToCode(block, "TEXT", Blockly.JavaScript.ORDER_ATOMIC);
     const replace = Blockly.JavaScript.valueToCode(block, "REPLACE", Blockly.JavaScript.ORDER_ATOMIC);
-    const withh = Blockly.JavaScript.valueToCode(block, "WITH", Blockly.JavaScript.ORDER_ATOMIC);
-    const code = [`(${text}|| '').replace((${replace}|| ''),(${withh}|| ''))`, Blockly.JavaScript.ORDER_NONE];
+    const replaceWith = Blockly.JavaScript.valueToCode(block, "WITH", Blockly.JavaScript.ORDER_ATOMIC);
+    const code = [`String(${text}).replace(new Regex(String(${replace}), 'g'), String(${replaceWith}))`, Blockly.JavaScript.ORDER_NONE];
     return code;
 };
 
