08e75976664eb4bdce9f5fd027fa420e101df7b3
jose_trindade1 <trindadej403@gmail.com>
2021-05-13 09:04:40 -0300

    updated replace and some translate things

--- src/blocks/text/replace.js
+++ src/blocks/text/replace.js
@@ -38,7 +38,7 @@ Blockly.JavaScript[blockName] = function(block) {
     const text = Blockly.JavaScript.valueToCode(block, "TEXT", Blockly.JavaScript.ORDER_ATOMIC);
     const replace = Blockly.JavaScript.valueToCode(block, "REPLACE", Blockly.JavaScript.ORDER_ATOMIC);
     const withh = Blockly.JavaScript.valueToCode(block, "WITH", Blockly.JavaScript.ORDER_ATOMIC);
-    const code = [`(${text}|| '').replace((${replace}|| ''),(${withh}|| ''))`, Blockly.JavaScript.ORDER_NONE];
+    const code = [`String(${text}).replace(String(${replace}),String(${withh}))`, Blockly.JavaScript.ORDER_NONE];
     return code;
 };
 
