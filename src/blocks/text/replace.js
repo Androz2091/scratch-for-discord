9e9386e01d4a409ffe03c2f589aab535f67d0262
Androz2091 <androz2091@gmail.com>
2021-05-13 14:12:50 +0200

    :bug: Fix replace block again

--- src/blocks/text/replace.js
+++ src/blocks/text/replace.js
@@ -38,7 +38,7 @@ Blockly.JavaScript[blockName] = function(block) {
     const text = Blockly.JavaScript.valueToCode(block, "TEXT", Blockly.JavaScript.ORDER_ATOMIC);
     const replace = Blockly.JavaScript.valueToCode(block, "REPLACE", Blockly.JavaScript.ORDER_ATOMIC);
     const replaceWith = Blockly.JavaScript.valueToCode(block, "WITH", Blockly.JavaScript.ORDER_ATOMIC);
-    const code = [`String(${text}).replace(new Regex(String(${replace}), 'g'), String(${replaceWith}))`, Blockly.JavaScript.ORDER_NONE];
+    const code = [`String(${text}).replace(new RegExp(String(${replace}), 'g'), String(${replaceWith}))`, Blockly.JavaScript.ORDER_NONE];
     return code;
 };
 
