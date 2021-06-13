2b3235c8b8fcacceac9cc385b7747004e0bb6f57
JollyBeaner <81916857+JollyBeaner@users.noreply.github.com>
2021-06-13 12:49:14 +0530

    Added advanced embed and advanced bot status

--- src/blocks/discord/base/set_bot_game.js
+++ src/blocks/discord/base/set_bot_game.js
@@ -5,16 +5,56 @@ const blockName = "s4d_set_bot_game";
 
 const blockData = {
     "message0": "%{BKY_SET_BOT_GAME}",
-    "args0": [
-        {
+    "args0": [{
             "type": "input_value",
             "name": "GAME",
-            "check": [ "Number", "String" ]
+            "check": ["Number", "String"]
+        },
+        {
+            "type": "field_dropdown",
+            "name": "TYPE",
+            "options": [
+                [
+                    "%{BKY_LISTENING}",
+                    "LISTENING"
+                ],
+                [
+                    "%{BKY_WATCHING}",
+                    "WATCHING"
+                ],
+                [
+                    "%{BKY_COMPETING}",
+                    "COMPETING"
+                ],
+                [
+                    "%{BKY_PLAYING}",
+                    "PLAYING"
+                ]
+            ]
+        },
+        {
+            "type": "field_dropdown",
+            "name": "OIFD",
+            "options": [
+                [
+                    "%{BKY_ONLINE}",
+                    "online"
+                ],
+                [
+                    "%{BKY_IDLE}",
+                    "idle"
+                ],
+                [
+                    "%{BKY_DND}",
+                    "dnd"
+                ]
+            ]
         },
     ],
     "colour": "#4C97FF",
     "previousStatement": null,
     "nextStatement": null,
+    "inputsInline": true,
     "tooltip": "",
     "helpUrl": ""
 };
@@ -25,18 +65,18 @@ Blockly.Blocks[blockName] = {
     }
 };
 
-Blockly.JavaScript[blockName] = function(block){
+Blockly.JavaScript[blockName] = function(block) {
+    const type = block.getFieldValue("TYPE");
     const game = Blockly.JavaScript.valueToCode(block, "GAME", Blockly.JavaScript.ORDER_ATOMIC);
-    const code = `s4d.client.user.setActivity(String(${game}));\n`;
+    const OIFD = block.getFieldValue("OIFD");
+    const code = `s4d.client.user.setPresence({status: "${OIFD}",activity:{name:${game},type:"${type}"}});; \n`;
     return code;
 };
 
-registerRestrictions(blockName, [
-    {
-        type: "notempty",
-        message: "RES_MISSING_GAME",
-        types: [
-            "GAME"
-        ]
-    }
-]);
+registerRestrictions(blockName, [{
+    type: "notempty",
+    message: "RES_MISSING_GAME",
+    types: [
+        "GAME"
+    ]
+}]);
