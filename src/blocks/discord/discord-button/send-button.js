904af7948d7f0faa7c77c67fec912dc41d0d7e10
jose_trindade1 <trindadej403@gmail.com>
2021-07-22 13:23:23 -0300

    updated send button
    now u can send embed

--- src/blocks/discord/discord-button/send-button.js
+++ src/blocks/discord/discord-button/send-button.js
@@ -13,7 +13,7 @@ const blockData = {
         {
             "type": "input_value",
             "name": "CONTENT",
-            "check": [ "String", "Number" ]  
+            "check": [ "String", "Number","MessageEmbed" ]  
         },
         {
             "type": "input_value",
@@ -36,8 +36,21 @@ Blockly.Blocks[blockName] = {
 
 Blockly.JavaScript[blockName] = function(block){
     const channel = Blockly.JavaScript.valueToCode(block, "CHANNEL", Blockly.JavaScript.ORDER_ATOMIC);
-    const content = Blockly.JavaScript.valueToCode(block, "CONTENT", Blockly.JavaScript.ORDER_ATOMIC);
     const button = Blockly.JavaScript.valueToCode(block, "BUTTON", Blockly.JavaScript.ORDER_ATOMIC);
-    const code = `${channel}.send(${content},${button})\n`;
-    return code
+    const content = Blockly.JavaScript.valueToCode(block, "CONTENT", Blockly.JavaScript.ORDER_ATOMIC);
+    if(block.getInput("CONTENT").connection.targetConnection){
+        const contentType = block.getInput("CONTENT").connection.targetConnection.getSourceBlock().outputConnection.check_ ?
+        block.getInput("CONTENT").connection.targetConnection.getSourceBlock().outputConnection.check_[0] :
+        null;
+        if((contentType === "MessageEmbed") || (!contentType && typeof contentType === "object")){
+            const code = `${channel}.send(${content},${button});\n`;
+            return code;
+        } else {
+            const code = `${channel}.send(String(${content}),${button});\n`;
+            return code;
+        }
+    } else {
+        const code = `${channel}.send(String(${content}),${button});\n`;
+        return code;
+    }
 };
