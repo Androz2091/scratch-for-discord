531f632d77159b7370794b294dfe02286d674651
mrredo-and-josetrindade1 <90334854+mrredo-and-josetrindade1@users.noreply.github.com>
2021-09-28 18:51:10 -0300

    fix

--- src/components/BlocklyComponent.vue
+++ src/components/BlocklyComponent.vue
@@ -97,34 +97,10 @@ export default {
 	color: rgb(204, 204, 204)
 }
 .dropdown-menu {
-    position: absolute;
-    top: 100%;
-    left: 0;
-    z-index: 1000;
-    display: none;
-    float: left;
-    min-width: 10rem;
-    padding: 0.5rem 0;
-    margin: 0.125rem 0 0;
-    font-size: 1rem;
     color: #212529;
-    text-align: left;
-    list-style: none;
     background-color: #000;
-    background-clip: padding-box;
-    border: 1px solid rgba(0, 0, 0, 0.15);
-    border-radius: 0.25rem;
 }
 .dropdown-item {
-    display: block;
-    width: 100%;
-    padding: 0.25rem 1.5rem;
-    clear: both;
-    font-weight: 400;
     color: #ffffff;
-    text-align: inherit;
-    white-space: nowrap;
-    background-color: transparent;
-    border: 0;
 }
 </style>
