ed82712f7df37e6dee4935f6ffab47c034dd60d7
frostzzone <65735427+frostzzone@users.noreply.github.com>
2021-11-08 08:23:38 -0800

    Update ExamplesMenu.vue
    Add image_gen example

--- src/components/NavigationBar/ExamplesMenu.vue
+++ src/components/NavigationBar/ExamplesMenu.vue
@@ -4,8 +4,7 @@
         <b-dropdown-item @click="load('command-parsing')">{{ $t('examples.command_parsing') }}</b-dropdown-item>
         <b-dropdown-item @click="load('leveling')">{{ $t('examples.leveling') }}</b-dropdown-item>
         <b-dropdown-item @click="load('music')">{{ $t('examples.music') }}</b-dropdown-item>
-        <b-dropdown-item @click="load('frostlevel')">{{ $t('examples.frostlevel') }}</b-dropdown-item>
-    </b-nav-item-dropdown>
+        <b-dropdown-item @click="load('image-gen')">{{ $t('examples.image_gen') }}</b-dropdown-item>
 </template>
 
 <script>
@@ -15,14 +14,14 @@ import PingPongExample from "../../examples/ping-pong";
 import CommandParsingExample from "../../examples/command-parsing";
 import LevelingExample from "../../examples/leveling";
 import MusicExample from "../../examples/music";
-import FrostLevelExample from "../../examples/frostlevel";
+import ImageGen from "../../examples/image_gen";
 
 const examples = {
     "ping-pong": PingPongExample,
     "command-parsing": CommandParsingExample,
     "leveling": LevelingExample,
     "music":MusicExample,
-    "frostlevel":FrostLevelExample
+    "image-gen": ImageGen
 };
 
 export default {
