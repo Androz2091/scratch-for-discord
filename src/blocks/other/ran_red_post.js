360df30d0c509ea8ff3c866800aae3a75d8673fb
XL83YT <80570235+XL83YT@users.noreply.github.com>
2021-11-11 10:39:57 +1000

    Update ran_red_post.js

--- src/blocks/other/ran_red_post.js
+++ src/blocks/other/ran_red_post.js
@@ -26,5 +26,21 @@ Blockly.Blocks[blockName] = {
 
 Blockly.JavaScript[blockName] = function(block){
     const message = Blockly.JavaScript.valueToCode(block, "MESSAGE", Blockly.JavaScript.ORDER_ATOMIC);
-    return `${message}`;
+    return ` const got = require('got')
+             let embed = new Discord.MessageEmbed();
+            got('https://www.reddit.com/r/' + ${message} + '/random/.json').then(response => {
+              let content = JSON.parse(response.body);
+              let permalink = content[0].data.children[0].data.permalink;
+              let memeUrl = `https://reddit.com${permalink}`;
+              let memeImage = content[0].data.children[0].data.url;
+              let memeTitle = content[0].data.children[0].data.title;
+              embed.setTitle('Click here to go to the post')
+              embed.setURL(memeUrl);
+              embed.setDescription(memeTitle);
+              embed.setImage(memeImage);
+              embed.setColor('RANDOM');
+              (s4dmessage.channel).send({
+                embeds: [embed]
+              });
+            })`;
 };
