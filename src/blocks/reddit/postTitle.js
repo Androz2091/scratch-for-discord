e2ad24291b6a12f6c3d5df994702bbfcc2cdef82
retrouser955 <70205403+retrouser955@users.noreply.github.com>
2021-11-12 15:56:58 +0630

    Create postTitle.js

---
+++ src/blocks/reddit/postTitle.js
@@ -0,0 +1,24 @@
+import * as Blockly from "blockly/core";
+
+const blockName = "post_Title";
+
+const blockData = {
+    "message0": "Post Title",
+    "colour": "#5ba58b",
+		"args0": [
+		],
+    "tooltip": null,
+    "output": "String",
+    "helpUrl": ""
+};
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function() {
+  const code = [`postTitle`, Blockly.JavaScript.ORDER_NONE];
+  return code;
+};
