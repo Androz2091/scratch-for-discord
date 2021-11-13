a8785b0bbf0d5e31f2dc038e2e9af1ee9b342a61
XL83YT <80570235+XL83YT@users.noreply.github.com>
2021-11-13 22:49:07 +1000

    Update ran_red_post.js

--- src/blocks/reddit/ran_red_post.js
+++ src/blocks/reddit/ran_red_post.js
@@ -3,20 +3,26 @@ import * as Blockly from "blockly/core";
 const blockName = "ran_red_post";
 
 
+
 const blockData = {
-    "message0": "Get random reddit post from r/%1",
+    "message0": "Get random reddit post from r/%1 then %2 %3",
     "args0": [
         {
-            "type": "input_value",
-            "name": "MESSAGE",
+            "type":"input_value",
+            "name":"MESSAGE",
             "check": [ "Number", "String" ]
+        },
+        {
+            "type": "input_dummy"
+        },
+        {
+            "type": "input_statement",
+            "name": "THEN"
         }
     ],
     "colour": "#5ba58b",
     "previousStatement": null,
     "nextStatement": null,
-    "tooltip": "",
-    "helpUrl": ""
 };
 
 
@@ -28,6 +34,7 @@ Blockly.Blocks[blockName] = {
 
 Blockly.JavaScript[blockName] = function(block){
     const message = Blockly.JavaScript.valueToCode(block, "MESSAGE", Blockly.JavaScript.ORDER_ATOMIC);
+    const statementThen = Blockly.JavaScript.statementToCode(block, "THEN");
     return ` const got = require('got')
              var embed = new Discord.MessageEmbed();
             got('https://www.reddit.com/r/' + ${message} + '/random/.json').then(response => {
@@ -36,5 +43,6 @@ Blockly.JavaScript[blockName] = function(block){
               var postUrl = 'https://reddit.com' + permalink;
               var postImage = content[0].data.children[0].data.url;
               var postTitle = content[0].data.children[0].data.title;
+               ${statementThen}
             })`;
 };
