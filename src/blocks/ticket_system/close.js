66ec78eb31d16ac6ec6d7f3163089f2390bd3b69
Retro <70205403+retrouser955@users.noreply.github.com>
2021-11-15 16:43:25 +0630

    Update close.js

--- src/blocks/ticket_system/close.js
+++ src/blocks/ticket_system/close.js
@@ -20,6 +20,6 @@ Blockly.Blocks[blockName] = {
 };
 
 Blockly.JavaScript[blockName] = function() {
-    const code = `s4dticket.close(s4dmessage.channel);`;
+    const code = `ticket.close(s4dmessage.channel);`;
     return code;
 };
