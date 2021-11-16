fd31a486dc276e7daee3acce9ca408854b3ff78f
XL83YT <80570235+XL83YT@users.noreply.github.com>
2021-11-16 12:46:39 +1000

    Create stats.js

---
+++ src/blocks/p-s_stats/stats.js
@@ -0,0 +1,41 @@
+import * as Blockly from "blockly";
+
+
+const blockName = "ec_name";
+
+const blockData = {
+    "message0": "%1",
+    "args0": [
+        {
+            "type": "field_dropdown",
+            "name": "INFO",
+            "options": [
+                [ "CPU Average Usage", "usage" ],
+                [ "CPU Model", "model" ],
+                [ "author", "author" ],
+                [ "animated", "animated" ],
+                [ "available", "available" ],
+                [ "deletable", "deletable" ],
+                [ "deleted", "deleted" ],
+                [ "url", "url" ],
+            ]
+        },
+    ],
+    "output": "String",
+    "colour": "#5BA58C",
+    "tooltip": "",
+    "helpUrl": ""
+};
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function(block) {
+    const info = block.getFieldValue("INFO");
+
+    const code = [`cpu.${info}()`, Blockly.JavaScript.ORDER_NONE];
+    return code;
+};
