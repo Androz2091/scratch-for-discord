73e4bf2bcff2d0c0b88af300652bbf1bd61f984d
XL83YT <80570235+XL83YT@users.noreply.github.com>
2021-11-23 15:17:17 +1000

    Create get_ps_stats.js

---
+++ src/blocks/p-s_stats/get_ps_stats.js
@@ -0,0 +1,26 @@
+import * as Blockly from "blockly/core";
+
+
+const blockName = "get_stats";
+
+const blockData = {
+    "message0": "Get Pc/Sever stats",
+    "args0": [],
+    "colour": "#D14081",
+    "previousStatement": null,
+    "nextStatement": null,
+    "tooltip": "",
+    "helpUrl": ""
+};
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function() {
+    const code = `
+       var os 	= require('os-utils');
+    return code;
+};
