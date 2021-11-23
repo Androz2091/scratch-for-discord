e78ca3011eb892f7d910255e5df763818abd874e
XL83YT <80570235+XL83YT@users.noreply.github.com>
2021-11-23 18:07:02 +1000

    Create cpu.js

---
+++ src/blocks/p-s_stats/cpu.js
@@ -0,0 +1,25 @@
+
+import Blockly from "blockly/core";
+
+const blockName = "cpu";
+
+const blockData = {
+    "message0": "Cpu usage",
+    "args0": [
+    ],
+    "output": "String",
+    "colour": "#40BF4A",
+    "tooltip": "",
+    "helpUrl": ""
+};
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function() {
+    const code = ['obj', Blockly.JavaScript.ORDER_NONE ];
+    return code;
+};
