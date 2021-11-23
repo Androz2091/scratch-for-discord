bea2b1e7f8aa83205e3998be4f5e10f347a42911
XL83YT <80570235+XL83YT@users.noreply.github.com>
2021-11-23 15:13:36 +1000

    Create get_cpu_uptime.js

---
+++ src/blocks/p-s_stats/get_cpu_uptime.js
@@ -0,0 +1,44 @@
+import * as Blockly from "blockly/core";
+
+const blockName = "get_cpu_uptime";
+
+const blockData = {
+    "message0": "Get CPU uptime then %2 %3",
+    "args0": [
+        {
+            "type": "input_dummy"
+        },
+        {
+            "type": "input_statement",
+            "name": "THEN"
+        }
+    ],
+    "colour": "#5ba58b",
+    "previousStatement": null,
+    "nextStatement": null,
+};
+
+
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function(block){
+
+    const statementThen = Blockly.JavaScript.statementToCode(block, "THEN");
+ 
+  const code = `
+        os.cpuUsage(function(v){
+	      var obj = v * 100
+        ${statementThen}   
+});
+                              
+                              
+        
+`;
+   
+    return code;
+};
