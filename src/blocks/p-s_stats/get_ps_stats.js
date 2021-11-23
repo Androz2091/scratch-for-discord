4b5a1a4e1919fa62996057bcf8c33f876e3ce42a
XL83YT <80570235+XL83YT@users.noreply.github.com>
2021-11-23 18:19:43 +1000

    Update get_ps_stats.js

--- src/blocks/p-s_stats/get_ps_stats.js
+++ src/blocks/p-s_stats/get_ps_stats.js
@@ -21,6 +21,6 @@ Blockly.Blocks[blockName] = {
 
 Blockly.JavaScript[blockName] = function() {
     const code = `
-       var os 	= require('os-utils');
+       var os = require('os-utils');
     return code;
 };
