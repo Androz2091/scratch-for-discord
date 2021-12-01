1ab9ab60f22c031c450b8e90de36c5d376cb21e0
Retro <70205403+retrouser955@users.noreply.github.com>
2021-12-01 10:10:32 +0630

    Update addedsong.js

--- src/blocks/better_music/addedsong.js
+++ src/blocks/better_music/addedsong.js
@@ -6,7 +6,7 @@ const blockData = {
     "message0": "Name of the added track",
     "args0": [
     ],
-    "output": "Channel",
+    "output": "String",
     "colour": "#a55b80",
     "tooltip": "",
     "helpUrl": ""
