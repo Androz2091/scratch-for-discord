f278a1e20bb03184e3d22d7afd44dbe317831700
RetroBTW <95496915+RetroBTW@users.noreply.github.com>
2021-12-08 03:07:09 -0300

    Update tts.js

--- src/blocks/TTS/tts.js
+++ src/blocks/TTS/tts.js
@@ -3,18 +3,24 @@ import * as Blockly from "blockly/core";
 const blockName = "tts-test";
 
 const blockData = {
-  "type": "input_value",
-  "message0": "Play OCR  %1 In channel  %2 Then",
+"message0": "Play OCR  %1 In channel  %2 Then %3 %4",
   "args0": [
     {
-      "type": "field_input",
+      "type": "input_value",
       "name": "ocr",
-      "text": "Hello!"
+      "check": "VoiceChannel"
+    },
+     {
+      "type": "input_value",
+      "name": "inchannel",
+      "check": "VoiceChannel"
     },
     {
-      "type": "input_value",
-      "name": "In channel",
-      "text": ""
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "THEN"
     }
   ],
   "inputsInline": false,
