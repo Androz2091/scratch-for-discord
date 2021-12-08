66553e2ff8174884b6f7436c274a868784437119
RetroBTW <95496915+RetroBTW@users.noreply.github.com>
2021-12-08 01:58:33 -0300

    Create ptts.js

---
+++ src/blocks/TTS/ptts.js
@@ -0,0 +1,23 @@
+import Blockly from "blockly/core";
+
+const blockName = "played-tts";
+
+const blockData = {
+    "message0": "Played TTS",
+    "args0": [
+    ],
+    "output": "String",
+    "colour": "#4287f5",
+    "tooltip": "",
+    "helpUrl": ""
+};
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function() {
+    return [`newSong.thumbnail`, Blockly.JavaScript.ORDER_NONE ];
+};
