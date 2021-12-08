ce30c7354513680fb3ffe6993fb3093f5b7c7ab3
RetroBTW <95496915+RetroBTW@users.noreply.github.com>
2021-12-08 17:57:40 -0300

    Create image_search.js

---
+++ src/blocks/Alex/image_search.js
@@ -0,0 +1,31 @@
+import * as Blockly from "blockly/core";
+
+const blockName = "search_image";
+
+const blockData = {
+  "type": "g",
+  "message0": "URL of image  %1",
+  "args0": [
+    {
+      "type": input_value"",
+      "name": "query",
+      "check": "String"
+    }
+  ],
+  "colour": "#06ccb2",
+  "tooltip": "",
+  "helpUrl": ""
+};
+
+Blockly.Blocks[blockName] = {
+  init: function () {
+    this.jsonInit(blockData);
+  }
+};
+
+Blockly.JavaScript[blockName] = function (block) {
+const query = Blockly.JavaScript.valueToCode(block, "query", Blockly.JavaScript.ORDER_ATOMIC);
+  const code =  `await  image_finder.find(${query})`;
+  return code;
+};
+
