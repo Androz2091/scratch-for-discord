c379fedbc488ff0ede859e3ded9849e184e72694
RetroBTW <95496915+RetroBTW@users.noreply.github.com>
2021-12-08 01:27:59 -0300

    Create tts.js

---
+++ src/blocks/other/tts.js
@@ -0,0 +1,44 @@
+import * as Blockly from "blockly/core";
+
+const blockName = "tts-test";
+
+const blockData = {
+  {
+  "type": "input_value",
+  "message0": "Play TTS  %1 In channel  %2 Then %3 %4",
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "TTS",
+      "text": "Hello!"
+    },
+    {
+      "type": "field_input",
+      "name": "In channel",
+      "text": ""
+    },
+    {
+      "type": "input_dummy",
+      "align": "RIGHT"
+    },
+    {
+      "type": "input_statement",
+      "name": "NAME",
+      "align": "RIGHT"
+    }
+  ],
+  "colour": "#49a4d1",
+  "tooltip": "",
+  "helpUrl": ""
+}
+
+Blockly.Blocks[blockName] = {
+  init: function () {
+    this.jsonInit(blockData);
+  }
+};
+
+Blockly.JavaScript[blockName] = function (block) {
+test;
+  return code;
+};
