053f8105db9b46cd32bb07cdf99684348edd2775
RetroBTW <95496915+RetroBTW@users.noreply.github.com>
2021-12-08 02:56:28 -0300

    Update toolbox.js

--- src/toolbox.js
+++ src/toolbox.js
@@ -454,6 +454,7 @@ export default (Blockly, value) => {
         <category name="TTS" colour="#49a4d1">
         <label text="Connect and use TTS" web-class="boldtext"></label>
         <block type="tts-test"/>
+	<block type="played-tts"/>
         </category>
         </category>
 	
