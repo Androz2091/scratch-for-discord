ea5fbcd5cf6f9da224a77d76a83206601b3a1f98
RetroBTW <95496915+RetroBTW@users.noreply.github.com>
2021-12-08 01:19:14 -0300

    Update toolbox.js

--- src/toolbox.js
+++ src/toolbox.js
@@ -501,6 +501,7 @@ export default (Blockly, value) => {
 <category name="Alex" colour="#00d1b5">
 	<category name="TTS" colour="#52a8de">
         </category>
+        </category>
 
 
 <category name="retro and xl83" colour="#AAE941">
