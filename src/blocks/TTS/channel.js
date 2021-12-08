02f49bff9eb83cbe12396cac061575ff99f4627b
RetroBTW <95496915+RetroBTW@users.noreply.github.com>
2021-12-08 16:15:04 -0300

    Update channel.js

--- src/blocks/TTS/channel.js
+++ src/blocks/TTS/channel.js
@@ -19,5 +19,5 @@ Blockly.Blocks[blockName] = {
 };
 
 Blockly.JavaScript[blockName] = function() {
-    return ['s4dmessage.member.voice.channelId', Blockly.JavaScript.ORDER_NONE ];
+    return 's4dmessage.member.voice.channelId';
 };
