7f55f2719120df105107602c1d8bf3c3976edc1f
AlexCdDg <95496915+AlexCdDg123@users.noreply.github.com>
2021-12-09 00:51:52 -0300

    Create tstart.js

---
+++ src/blocks/Alex/tstart.js
@@ -0,0 +1,30 @@
+	import * as Blockly from "blockly/core";
+
+const blockName = "typing_start";
+
+const blockData = {
+"message0": "When someone starts typing %1 %2",
+  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "THEN"
+    }
+  ],
+  "colour": "#f79400",
+  "tooltip": "",
+  "helpUrl": ""
+}
+Blockly.Blocks[blockName] = {
+  init: function () {
+    this.jsonInit(blockData);
+  }
+};
+
+Blockly.JavaScript[blockName] = function (block) {
+const then = Blockly.JavaScript.valueToCode(block, then", Blockly.JavaScript.ORDER_ATOMIC);
+  const code =  `s4d.client.on('typingStart', async (s4dTyping) => { \n ${then}`;
+  return code;
+};
