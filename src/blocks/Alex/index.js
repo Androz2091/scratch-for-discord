d4b287c22b63b49b1568bca76e0c3aceb409e90a
AlexCdDg <95496915+AlexCdDg123@users.noreply.github.com>
2021-12-09 01:38:12 -0300

    Update index.js

--- src/blocks/Alex/index.js
+++ src/blocks/Alex/index.js
@@ -2,6 +2,7 @@ import "./tts";
 import "./image_search";
 import "./url";
 import "./tstart";
-import "./tchannel"
-import "./tmember"
+import "./tchannel";
+import "./tmember";
+import "./tserver";
 import "./channel"
