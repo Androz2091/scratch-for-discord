2f664f2cd38a384a9625f26030633df6c849cc9a
AlexCdDg <95496915+AlexCdDg123@users.noreply.github.com>
2021-12-10 23:07:34 -0300

    Create channel_perms.js

---
+++ src/blocks/Alex/channel_perms.js
@@ -0,0 +1,98 @@
+import Blockly from "blockly/core";
+
+const blockName = "channel_perms";
+
+const blockData = {
+"message0": "%1  permission  %2 %3 to role / member / everyone  %4 in channel  %5",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "ad",
+      "options": [
+        [
+          "Allow",
+          "true"
+        ],
+        [
+          "Deny",
+          "false"
+        ]
+      ]
+    },
+    {
+      "type": "field_dropdown",
+      "name": "perm",
+      "options": [
+        [
+          "CREATE_INSTANT_INVITE",
+          "CREATE_INSTANT_INVITE"
+        ],
+        [
+          "VIEW_CHANNEL",
+          "VIEW_CHANNEL"
+        ],
+        [
+          "SEND_MESSAGES",
+          "SEND_MESSAGES"
+        ],
+        [
+          "SEND_TTS_MESSAGES",
+          "SEND_TTS_MESSAGES"
+        ],
+        [
+          "MANAGE_MESSAGES",
+          "MANAGE_MESSAGES"
+        ],
+        [
+          "EMBED_LINKS",
+          "EMBED_LINKS"
+        ],
+        [
+          "ATTACH_FILES",
+          "ATTACH_FILES"
+        ],
+        [
+          "READ_MESSAGE_HISTORY",
+          "READ_MESSAGE_HISTORY"
+        ],
+        [
+          "MENTION_EVERYONE",
+          "MENTION_EVERYONE"
+        ]
+      ]
+    },
+    {
+      "type": "input_space"
+    },
+    {
+      "type": "input_value",
+      "name": "to",
+      "check": ["Member", "Role", "Everyone"]
+    },
+    {
+      "type": "input_value",
+      "name": "channel",
+      "check": "Channel"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": "#0c97f0",
+  "tooltip": "",
+  "helpUrl": ""
+}
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function(block) {
+const trufal = block.getFieldValue("ad");
+const perm = block.getFieldValue("perm");
+const to = Blockly.JavaScript.valueToCode(block, "to", Blockly.JavaScript.ORDER_ATOMIC);
+const channel = Blockly.JavaScript.valueToCode(block, "channel", Blockly.JavaScript.ORDER_ATOMIC);
+    const code = `${channel}.permissionOverwrites.edit(${to}, { ${perm}: ${trufal} });`;
+    return code;
+};
