d6740055a3940b01b3db7ce7e45410421b16bfc0
AlexCdDg <95496915+AlexCdDg123@users.noreply.github.com>
2021-12-10 23:07:52 -0300

    Update index.js

--- src/blocks/Alex/index.js
+++ src/blocks/Alex/index.js
@@ -9,6 +9,7 @@ import "./higrole";
 import "./isdm";
 import "./colorofmember";
 import "./channel_category";
+import "./channel_perms";
 import "./vchannel_category";
 import "./renamec";
 import "./channel"
