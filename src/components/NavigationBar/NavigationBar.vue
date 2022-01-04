e7f25fc2068e97c99b9c82b880c73dcdde79e505
AHQ Miness <ahqsecret@gmail.com>
2022-01-04 21:53:29 +0530

    updated d.js

--- src/components/NavigationBar/NavigationBar.vue
+++ src/components/NavigationBar/NavigationBar.vue
@@ -194,7 +194,7 @@ load()`);
                         "dependencies": {\n
                              "djs-games": "^2.1.10",\n
                             "lyrics-finder": "^21.7.0",\n
-                            "discord.js": "^13.3.1",\n
+                            "discord.js": "latest",\n
                             "os-utils": "0.0.14",\n
                             "easy-json-database": "^1.5.0",\n
                             "moment": "latest",\n
