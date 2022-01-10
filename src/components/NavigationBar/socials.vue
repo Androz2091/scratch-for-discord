afd70993d08564bf81854dbdde61986272703797
XL83YT <80570235+XL83YT@users.noreply.github.com>
2022-01-10 17:09:52 +1000

    Update socials.vue

--- src/components/NavigationBar/socials.vue
+++ src/components/NavigationBar/socials.vue
@@ -1,7 +1,7 @@
 <template>
      <b-nav-item-dropdown :text="$t('Socials')" right>
-        <b-dropdown-item href="https://www.reddit.com/r/s4d/" target="_blank">{{$t('S4D Reddit')}}</b-dropdown-item>
-        <b-dropdown-item href="https://scratch-for-discord.fandom.com/wiki/Scratch_For_Discord_Wiki" target="_blank">{{$t('S4D Wiki')}}</b-dropdown-item>
-          <b-dropdown-item href="https://dsc.gg/s4d" target="_blank">{{$t('S4D Discord Server')}}</b-dropdown-item>
+        <b-dropdown-item href="https://www.reddit.com/r/s4d/" target="_blank">{{$t('Reddit')}}</b-dropdown-item>
+        <b-dropdown-item href="https://scratch-for-discord.fandom.com/wiki/Scratch_For_Discord_Wiki" target="_blank">{{$t('Wiki')}}</b-dropdown-item>
+          <b-dropdown-item href="https://dsc.gg/s4d" target="_blank">{{$t('Discord Server')}}</b-dropdown-item>
     </b-nav-item-dropdown>
 </template>
