620587920f072f0f75b21c7e4eb40fc4f9bbec32
XL83YT <80570235+XL83YT@users.noreply.github.com>
2022-01-10 17:01:14 +1000

    Create socials.vue

---
+++ src/components/NavigationBar/socials.vue
@@ -0,0 +1,7 @@
+<template>
+     <b-nav-item-dropdown :text="$t('Socials')" right>
+        <b-dropdown-item href="https://www.reddit.com/r/s4d/" target="_blank">{{$t('S4D Reddit')}}</b-dropdown-item>
+        <b-dropdown-item href="https://scratch-for-discord.fandom.com/wiki/Scratch_For_Discord_Wiki" target="_blank">{{$t('S4D Wiki')}}</b-dropdown-item>
+          <b-dropdown-item href="https://dsc.gg/s4d" target="_blank">{{$t('S4D Discord Server')}}</b-dropdown-item>
+    </b-nav-item-dropdown>
+</template>
