3537b64e29b5331d600eda643ea1ae2d347794cc
frostzzone <65735427+frostzzone@users.noreply.github.com>
2022-01-14 19:51:50 +0000

    fix [nickname] of massage author

--- src/blocks/discord/message-event/message_author_raw.js
+++ src/blocks/discord/message-event/message_author_raw.js
@@ -45,7 +45,7 @@ Blockly.JavaScript[blockName] = function(block) {
         const code = ["s4dmessage.member.user.username", Blockly.JavaScript.ORDER_NONE];
         return code;
     } else if(searchType === "NICKNAME"){
-        const code = ["(s4dmessage.member.user.username || await s4dmessage.guild.members.fetch(s4dmessage.author.id)).user.nickname", Blockly.JavaScript.ORDER_NONE];
+        const code = ["(s4dmessage.member || await s4dmessage.guild.members.fetch(s4dmessage.author.id)).nickname", Blockly.JavaScript.ORDER_NONE];
         return code;
     }
 };
