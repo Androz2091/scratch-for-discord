62e32101aafac6bfff30a05defe41964f9b67461
Eruption <94153406+EruptionGuy@users.noreply.github.com>
2022-02-07 10:11:41 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -286,7 +286,7 @@ export default {
 
 <style>
 #app {
-        font-family: "Oswald", sans-serif;
+        font-family: sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
