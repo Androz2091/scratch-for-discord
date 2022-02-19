891319f63f6ada23722c1acccd286ea1af2a70b4
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-02-20 01:39:16 +1000

    Update require.js

--- src/require.js
+++ src/require.js
@@ -132,14 +132,15 @@ if(js.includes("s4d.client.discordTogether")){
     array.push(`const SnakeGame = require('snakecord')`)
 }
  if(js.includes("os.sysUptime()")){
-    code.push(`function secondsToDhms(seconds) {
-var days = Math.floor(seconds / (3600*24));
-seconds  -= days*3600*24;
-var hrs   = Math.floor(seconds / 3600);
-seconds  -= hrs*3600;
-var mnts = Math.floor(seconds / 60);
-seconds  -= mnts*60;
-return Math.floor(days)+" days, "+Math.floor(hrs)+" Hrs, "+Math.floor(mnts)+" Minutes, "+Math.floor(seconds)+" Seconds"
+    code.push(`function dhm(ms) {
+  const days = Math.floor(ms / (24*60*60*1000));
+  const daysms = ms % (24*60*60*1000);
+  const hours = Math.floor(daysms / (60*60*1000));
+  const hoursms = ms % (60*60*1000);
+  const minutes = Math.floor(hoursms / (60*1000));
+  const minutesms = ms % (60*1000);
+  const sec = Math.floor(minutesms / 1000);
+  return days+" days, "+hours+" Hrs, "+minutes+" Minutes, "+sec+" Seconds"
 }`)
 }    
 if(js.includes("Cooldown")){
