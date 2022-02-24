5281806b81ecfd1d6f6df0af2cc80587a044b9a7
frostzzone <65735427+frostzzone@users.noreply.github.com>
2022-02-24 17:39:16 +0000

    fix on deleted warnnig from server icon/id/name/owner

--- src/blocks/discord/bot-event/server_name.js
+++ src/blocks/discord/bot-event/server_name.js
@@ -27,7 +27,8 @@ registerRestrictions(blockName, [
         type: "toplevelparent",
         message: "RES_MUST_BE_IN_ON_ADDED",
         types: [
-            "s4d_on_added"
+            "s4d_on_added",
+            "s4d_on_remove"
         ]
     }
 ]);
