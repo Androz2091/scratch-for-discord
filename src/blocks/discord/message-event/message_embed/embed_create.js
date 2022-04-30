0f94da22d5fd2b66fcbc39d6c80defa6bceb38a7
Eruption <94153406+EruptionGuy@users.noreply.github.com>
2022-04-30 16:58:23 +0700

    Create embed_create.js

---
+++ src/blocks/discord/message-event/message_embed/embed_create.js
@@ -0,0 +1,33 @@
+import * as Blockly from "blockly/core";
+
+const blockName = "s4d_embed_create";
+
+const blockData = {
+    "message0": "create an embed %1 then %2",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_statement",
+        "name": "THEN"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#40BF4A",
+    "tooltip": "Create a perfect embed.",
+    "helpUrl": ""
+};
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function(block){
+    const then = Blockly.JavaScript.statementToCode(block, "THEN");
+    const code = `let embed = new Discord.MessageEmbed() \n ${then}\n`;
+    return code;
+};
