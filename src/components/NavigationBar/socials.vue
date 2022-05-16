b593178a8d5f4f3384069e3ed7dbb63f1f6b20de
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2022-05-16 17:54:09 -0600

    that cringly fringly
    the skrinkly prinkly

--- src/components/NavigationBar/socials.vue
+++ src/components/NavigationBar/socials.vue
@@ -6,5 +6,6 @@
         <b-dropdown-item href="https://www.reddit.com/r/s4d/" target="_blank">{{$t('Reddit')}}</b-dropdown-item>
         <b-dropdown-item href="https://scratch-for-discord.fandom.com/wiki/Scratch_For_Discord_Wiki" target="_blank">{{$t('Wiki')}}</b-dropdown-item>
           <b-dropdown-item href="https://dsc.gg/s4d" target="_blank">{{$t('Discord Server')}}</b-dropdown-item>
+          <b-dropdown-item href="https://slash-commands-gui.androz2091.fr/settings" target="_blank">{{$t('Slash Register')}}</b-dropdown-item>
     </b-nav-item-dropdown>
 </template>
