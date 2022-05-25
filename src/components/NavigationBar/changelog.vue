4a3458ac33766841181915383f5e1c699a16f352
ItsLimeNade <88803242+ItsLimeNade@users.noreply.github.com>
2022-05-26 00:51:53 +0200

    yey

--- src/components/NavigationBar/changelog.vue
+++ src/components/NavigationBar/changelog.vue
@@ -14,12 +14,8 @@ export default {
                 position: 'center',
                 icon: 'info',
                 width: "700px",
-                title: 'Scheduled Events!',
-                text: 'Added scheduled events! It will we updated soon!',
-                imageUrl: 'https://cdn.discordapp.com/attachments/898783648491397191/979041343542267954/unknown.png?size=4096',
-                imageWidth: 475,
-                imageHeight: 519,
-                imageAlt: 'Custom image',
+                html:"<h2>You can now use alt+t and alt+n to spawn a text or number block!</h2> <br> <h2><b>Scheduled Events!</b></h2><br><img src='https://cdn.discordapp.com/attachments/898783648491397191/979041343542267954/unknown.png?size=4096' width='475' height='519'> <br>" +
+                " <h3><b>Added scheduled events! It will we updated soon!</b></h3>",
                 confirmButtonText:
                 '<i class="fa fa-thumbs-up"></i> Great!',
                 confirmButtonAriaLabel: 'Thumbs up, great!',
