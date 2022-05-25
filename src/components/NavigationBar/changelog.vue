d107e4553a6ca95af2b111e93c53e04a87de29a6
ItsLimeNade <88803242+ItsLimeNade@users.noreply.github.com>
2022-05-25 17:35:58 +0200

    added changelog thing

---
+++ src/components/NavigationBar/changelog.vue
@@ -0,0 +1,30 @@
+<template>
+        <b-nav-item @click="changelog">Changelog</b-nav-item>
+</template>
+
+
+
+<script>
+import Swal from "sweetalert2";
+export default {
+    name: "Changelog",
+    methods: {
+        changelog() {
+            Swal.fire({
+                position: 'center',
+                icon: 'info',
+                width: "700px",
+                title: 'Scheduled Events!',
+                text: 'Added scheduled events! It will we updated soon!',
+                imageUrl: 'https://cdn.discordapp.com/attachments/898783648491397191/979041343542267954/unknown.png?size=4096',
+                imageWidth: 475,
+                imageHeight: 519,
+                imageAlt: 'Custom image',
+                confirmButtonText:
+                '<i class="fa fa-thumbs-up"></i> Great!',
+                confirmButtonAriaLabel: 'Thumbs up, great!',
+            })
+        },
+    }
+}
+</script>
