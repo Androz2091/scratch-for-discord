bd09c16dda43e9022bb535ffd9edd1c3b2815932
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2022-06-08 05:58:18 -0600

    add embed into edit message

--- src/blocks/discord/message-event/edit_msg_by_id.js
+++ src/blocks/discord/message-event/edit_msg_by_id.js
@@ -5,20 +5,20 @@ const blockName = "edit_msg_by_id";
 const blockData = {
     "message0": "Get message with the id of %1 in the channel %2 and edit it to %3",
     "args0": [{
-            "type": "input_value",
-            "name": "ID",
-            "check": ["Number", "String"]
-        },
-        {
-            "type": "input_value",
-            "name": "CHANNEL",
-            "check": "Channel"
-        },
-        {
-            "type": "input_value",
-            "name": "EDIT",
-            "check": "String"
-        },
+        "type": "input_value",
+        "name": "ID",
+        "check": ["Number", "String"]
+    },
+    {
+        "type": "input_value",
+        "name": "CHANNEL",
+        "check": "Channel"
+    },
+    {
+        "type": "input_value",
+        "name": "EDIT",
+        "check": ["String", "Embed", "MessageEmbed", "embed", "Messageembed", "messageembed"]
+    },
     ],
     "colour": "#4C97FF",
     "previousStatement": null,
@@ -29,18 +29,23 @@ const blockData = {
 };
 
 Blockly.Blocks[blockName] = {
-    init: function() {
+    init: function () {
         this.jsonInit(blockData);
     }
 };
 
-Blockly.JavaScript[blockName] = function(block) {
+Blockly.JavaScript[blockName] = function (block) {
     const channel = Blockly.JavaScript.valueToCode(block, "CHANNEL", Blockly.JavaScript.ORDER_ATOMIC);
     const id = Blockly.JavaScript.valueToCode(block, "ID", Blockly.JavaScript.ORDER_ATOMIC);
-    const edit = Blockly.JavaScript.valueToCode(block, "EDIT", Blockly.JavaScript.ORDER_ATOMIC);
+    let edit = Blockly.JavaScript.valueToCode(block, "EDIT", Blockly.JavaScript.ORDER_ATOMIC);
+    if (!(String(edit).includes("embeds: ["))) {
+        edit = `content: String(${edit})`
+    } else {
+        edit = edit.replaceAll("{", "").replaceAll("(", "").replaceAll("}", "").replaceAll(")", "")
+    }
     const code = `${channel}.messages.fetch(${id}).then(async (msg) => {
        msg.edit({
-                    content: String(${edit})
+                    ${edit}
                 })
 })
 `;
