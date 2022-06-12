4c4e50fe701d9b38cc335bd1b30883b86d453a01
Cat Soup <98920372+CatSoup1@users.noreply.github.com>
2022-06-12 20:41:37 +1000

    REALLY REALLY REALLY IMPORTANT UPDATE

--- src/App.vue
+++ src/App.vue
@@ -188,6 +188,7 @@ export default {
             "color:red;font-family:system-ui;font-size:2rem;-webkit-text-stroke: 1px black;font-weight:bold",
             "color:DarkRed;font-family:system-ui;font-size:2rem;-webkit-text-stroke: 1px black;font-weight:bold"
         )
+	console.log("meow")
         if (!("ScratchNative" in window) && window.parent?.ScratchNative) window.ScratchNative = window.parent.ScratchNative;
         const tourDone = localStorage.getItem('tourDone');
         if (tourDone !== null) this.$store.commit('setTour', {
