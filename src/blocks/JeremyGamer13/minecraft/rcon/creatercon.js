8add7799a9b47efdb5b9472e40ed707a2a4479fb
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2022-06-11 18:42:02 -0600

    fard

--- src/blocks/JeremyGamer13/minecraft/rcon/creatercon.js
+++ src/blocks/JeremyGamer13/minecraft/rcon/creatercon.js
@@ -6,14 +6,14 @@ const blockData = {
     "message0": "create RCON client and connect to IP %1 using RCON port %2 with password %5 then %3 %4",
     "args0": [
         {
-            "type": "input_value", 
+            "type": "input_value",
             "name": "IP",
-            "check": ["String","var","Env"]
+            "check": ["String", "var", "Env"]
         },
         {
-            "type": "input_value", 
+            "type": "input_value",
             "name": "PORT",
-            "check": ["String","var","Env","Number"]
+            "check": ["String", "var", "Env", "Number"]
         },
         {
             "type": "input_dummy"
@@ -23,11 +23,11 @@ const blockData = {
             "name": "THEN"
         },
         {
-            "type": "input_value", 
+            "type": "input_value",
             "name": "PASSWORD",
-            "check": ["String","var","Env","Number"]
+            "check": ["String", "var", "Env", "Number"]
         }
-        
+
     ],
     "colour": 160,
     "previousStatement": null,
@@ -37,12 +37,12 @@ const blockData = {
 };
 
 Blockly.Blocks[blockName] = {
-    init: function() {
+    init: function () {
         this.jsonInit(blockData);
     }
 };
 
-Blockly.JavaScript[blockName] = function(block) {
+Blockly.JavaScript[blockName] = function (block) {
     const ip = Blockly.JavaScript.valueToCode(block, "IP", Blockly.JavaScript.ORDER_ATOMIC);
     const port = Blockly.JavaScript.valueToCode(block, "PORT", Blockly.JavaScript.ORDER_ATOMIC);
     const password = Blockly.JavaScript.valueToCode(block, "PASSWORD", Blockly.JavaScript.ORDER_ATOMIC);
@@ -51,8 +51,8 @@ Blockly.JavaScript[blockName] = function(block) {
         timeout: 5000
     };
     var S4D_APP_MC_RCON_CLIENT = new S4D_APP_MC_GET.RCON();
-    await client.connect(String(${ip}), Number(${port}), S4D_APP_MC_RCON_CLIENT_OPTIONS);
-    await client.login(String(${password}), S4D_APP_MC_RCON_CLIENT_OPTIONS);
+    await S4D_APP_MC_RCON_CLIENT.connect(String(${ip}), Number(${port}), S4D_APP_MC_RCON_CLIENT_OPTIONS);
+    await S4D_APP_MC_RCON_CLIENT.login(String(${password}), S4D_APP_MC_RCON_CLIENT_OPTIONS);
     ${then}
     `;
     return code;
