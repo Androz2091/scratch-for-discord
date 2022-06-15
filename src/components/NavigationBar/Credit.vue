3b1c0a31dee8e6326d3537648d54931a9837014c
User <alexandre.zarkua@gmail.com>
2022-06-15 13:06:39 +0200

    Made it so devs are better than other people hahaahahahHAHhHAHAHHA

--- src/components/NavigationBar/Credit.vue
+++ src/components/NavigationBar/Credit.vue
@@ -15,8 +15,9 @@ export default {
                 width: "1000px",
                 html: `
                 <h1><span>Developers of </span><span style="color:#15a9e8">Scratch For Discord!</span></h1>
-                    <h2>Preview 469:</h2>
+                    <h2><b>Preview 469:</b></h2>
                     <b><p>
+                    <h2><b>Developers:</b></h2>
                     <span style="color:#2aa9bd">frostzzone</span>,
                     <span style="color:#ad6f39">RetroBTW</span>,
                     <span style="color:#73ad39">cat soup</span>,
@@ -27,17 +28,18 @@ export default {
                     <span style="color:#e3aa24">AlexCdDg</span>,
                     <span style="color:#e88f68">Eruption</span>,
                     <span style="color:#429139">jose_trindade</span>,
-                    <span style="color:#9c2525">MrRedo</span>,
+                    <span style="color:#9c2525">MrRedo</span> <br><br> 
+                    <h2><b>Contributors:</b></h2>
                     <span style="color:#8B48A3">LaserCat</span>,
                     <span style="color:#ff7b00">Parham</span>,
                     <span style="color:#69b0d6">Monaco</span>
                     </p></b>
-                    <h3>Original S4D:</h3>
+                    <h3><b>Original S4D:</b></h3>
                     <b><p>
                     <span style="color:#36915a">Androz</span>,
                     <span style="color:#568896">snowflake107</span>
                     </p></b>
-                    <h3>Preview 454:</h3>
+                    <h3><b>Preview 454:</b></h3>
                     <b><p>
                     <span style="color:#8f815b">7coil</span>,
                     <span style="color:#429139">jose_trindade</span>,
