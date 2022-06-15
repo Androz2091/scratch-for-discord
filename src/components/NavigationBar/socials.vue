e6469214537a10b198117e343e03e46d28e62cb1
Eruption <94153406+EruptionGuy@users.noreply.github.com>
2022-06-15 21:58:08 +0700

    Literally just changed the name

--- src/components/NavigationBar/socials.vue
+++ src/components/NavigationBar/socials.vue
@@ -1,6 +1,5 @@
 <template>
-     <b-nav-item-dropdown :text="`Slash,
-       Socials & Host`" right>
+     <b-nav-item-dropdown :text="`Important Links`" right>
        <!--<b-dropdown-item href="https://slash-commands-gui.androz2091.fr/settings" target="_blank">{{$t('Slash Register')}}</b-dropdown-item>-->
        <b-dropdown-item href="https://discord.gg/nJhYCSddVy" target="_blank">Simple Host</b-dropdown-item>
         <b-dropdown-item href="https://www.reddit.com/r/s4d/" target="_blank">Reddit</b-dropdown-item>
