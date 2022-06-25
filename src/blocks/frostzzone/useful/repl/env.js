952e997426ad796523e154530d2bcda8376a218f
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2022-06-24 23:00:02 -0600

    g866f765dtfuhjb

--- src/blocks/frostzzone/useful/repl/env.js
+++ src/blocks/frostzzone/useful/repl/env.js
@@ -2,29 +2,29 @@ import * as Blockly from "blockly/core";
 
 const blockName = "frost_env";
 const blockData = {
-    "message0": "process.env. %1",
-    "colour": "#3333ff",
-		"args0": [
-        {
-          "type": "input_value",
-          "name": "VALUE",
-          "check": "String"
-        },
-		],
-    "tooltip": null,
-    "output": "String",
-    "helpUrl": ""
+  "message0": "process.env.%1",
+  "colour": "#3333ff",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "VALUE",
+      "check": "String"
+    },
+  ],
+  "tooltip": null,
+  "output": "String",
+  "helpUrl": ""
 };
 
 Blockly.Blocks[blockName] = {
-    init: function() {
-        this.jsonInit(blockData);
-    }
+  init: function () {
+    this.jsonInit(blockData);
+  }
 };
 
-Blockly.JavaScript[blockName] = function(block) {
-	const value = Blockly.JavaScript.valueToCode(block, "VALUE", Blockly.JavaScript.ORDER_ATOMIC);
-	const replacedValue = value.replace("'",'').replace("'",'')
+Blockly.JavaScript[blockName] = function (block) {
+  const value = Blockly.JavaScript.valueToCode(block, "VALUE", Blockly.JavaScript.ORDER_ATOMIC);
+  const replacedValue = value.replace("'", '').replace("'", '')
   const code = [`process.env.${replacedValue}`, Blockly.JavaScript.ORDER_NONE];
   return code;
 };
