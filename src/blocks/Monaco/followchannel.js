6f223bc7d5f37ff6857ec7b508a76629460bdf18
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2022-06-29 10:07:57 -0600

    fix

--- src/blocks/Monaco/followchannel.js
+++ src/blocks/Monaco/followchannel.js
@@ -4,10 +4,10 @@ const blockName = "monaco_toggle_follow_channel";
 
 
 const blockData = {
-    "message0": "follow channel %1 to channel %2 with reason %3",
-    "inputsInline": true,
-    "args0": [
-       {
+  "message0": "follow channel %1 to channel %2 with reason %3",
+  "inputsInline": true,
+  "args0": [
+    {
       "type": "input_value",
       "name": "togglechannel",
       "check": "Channel"
@@ -22,25 +22,26 @@ const blockData = {
       "name": "reason",
       "check": "String"
     }
-    ],
-    "colour": "#0c97f0",
-"previousStatement": null,
-"nextStatement": null,
-    "tooltip": "Follow news channels.",
-    "helpUrl": ""
+  ],
+  "colour": "#0c97f0",
+  "previousStatement": null,
+  "nextStatement": null,
+  "tooltip": "Follow news channels.",
+  "helpUrl": ""
 };
 
 Blockly.Blocks[blockName] = {
-    init: function() {
-        this.jsonInit(blockData);
-    }
+  init: function () {
+    this.jsonInit(blockData);
+  }
 };
 
-Blockly.JavaScript[blockName] = function(block) {
+Blockly.JavaScript[blockName] = function (block) {
   var value_togglechannel = Blockly.JavaScript.valueToCode(block, 'togglechannel', Blockly.JavaScript.ORDER_ATOMIC);
   var value_locationchannel = Blockly.JavaScript.valueToCode(block, 'locationchannel', Blockly.JavaScript.ORDER_ATOMIC);
   var value_reason = Blockly.JavaScript.valueToCode(block, 'reason', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = ` ${value_togglechannel}.addFollower(${value_locationchannel}, String(${value_reason})\n`;
+  var code = ` ${value_togglechannel}.addFollower(${value_locationchannel}, String(${value_reason}))
+`;
   return code;
 };
