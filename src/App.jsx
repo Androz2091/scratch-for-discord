445804adc0b1477a3d8d35607d32361f9234efb6
Retro <htoomyataung382@gmail.com>
2022-06-30 13:10:34 +0630

    generate javascript code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css';
-import { useCallback } from 'react';
-import toolbox from './toolbox';
-import DarkTheme from "@blockly/theme-dark";
+import './App.css'
+import { useCallback, /**, useState **/useRef } from 'react'
+import toolbox from './toolbox'
+import DarkTheme from "@blockly/theme-dark"
 import './blocks/discord/base'
-import Blockly from 'blockly/core'
-import Navbar from './components/Navbar'
+import * as Blockly from 'blockly/core'
+// import Navbar from './components/Navbar'
+import javascript from 'blockly/javascript'
 
-let workspace
 function App() {
+  // const [isDarkmode, setIsDarkmode] = useState(Boolean(localStorage.getItem('darkmode')) || false)
+  let workspace = useRef()
   const blocklyParent = useCallback((e) => {
     const blocklyDiv = document.createElement('div')
     blocklyDiv.classList.add('h-full')
     blocklyDiv.classList.add('w-full')
     e.append(blocklyDiv)
-    workspace = Blockly.inject(blocklyDiv, {
+    workspace.current = Blockly.inject(blocklyDiv, {
       toolbox: toolbox,
       theme: DarkTheme,
       renderer: 'zelos',
@@ -36,14 +38,61 @@ function App() {
         drag: true,
         wheel: true
       },
-      CSS: false
+      CSS: false,
     })
-  }, [])
+  }, [/**isDarkmode**/])
   return (
     <>
-      <Navbar />
+      <nav className="flex items-center justify-between flex-wrap bg-gray-1000 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6 my-auto">
+            <img src="https://cdn.discordapp.com/attachments/932651845447483428/989379701795594300/unknown.png" alt="scratch" className="fill-current h-10 w-10 mr-2"/>
+            <span className="font-semibold text-xl tracking-tight">Scratch For Discord</span>
+        </div>
+        <div className="block lg:hidden">
+        </div>
+            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+              <div className="text-sm lg:flex-grow">
+                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4">
+                    File
+                </a>
+                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4">
+                    Edit
+                </a>
+                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-white">
+                    Toolbox
+                </a>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition-all ml-4" onClick={() => {
+                  const code = `
+                  (async () => {
+                    const Discord = require("discord.js")
+                    const devMode = typeof __E_IS_DEV !== "undefined" && __E_IS_DEV
+                    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+                    const s4d = {
+                        Discord,
+                        joiningMember: null,
+                        reply: null,
+                        tokenInvalid: false,
+                        tokenError: null,
+                        checkMessageExists() {
+                          if (!s4d.client) throw new Error('You cannot perform message operations without a Discord.js client')
+                          if (!s4d.client.readyTimestamp) throw new Error('You cannot perform message operations while the bot is not connected to the Discord API')
+                        }
+                    }
+                    s4d.client = new s4d.Discord.Client({
+                      intents: [Object.values(s4d.Discord.Intents.FLAGS).reduce((acc, p) => acc | p, 0)],
+                      partials: ["REACTION"]
+                    })
+                    ${javascript.workspaceToCode(workspace.current)}
+                    return s4d
+                  })()
+                  `
+                  console.log(code)
+                }}>Generate Code</button>
+            </div>
+        </div>
+      </nav>
       <div ref={blocklyParent} className="h-screen w-full"></div>
     </>
   )
 }
-export default App;
+export default App
