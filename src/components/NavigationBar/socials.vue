59c13537d4fba8179dc8fc628c2111ce6f9a4839
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-07-06 20:21:41 -0700

    added the main s4d servers to important links
    ISGRHINGUBFVTBYGNUHSSSSSSSSSSSSSSSSGOFIB9YDSGBFOIGYVNDUHGIHUNDSM,OGHDFUMNJIGHK9DFVHNJCDMSAJKHN AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/components/NavigationBar/socials.vue
+++ src/components/NavigationBar/socials.vue
@@ -7,5 +7,9 @@
     <b-dropdown-item href="https://slash-commands-gui.androz2091.fr/settings" target="_blank">Slash Register</b-dropdown-item>
     <b-dropdown-item href="https://discord.gg/nJhYCSddVy" target="_blank">Simple Host</b-dropdown-item>
     <b-dropdown-item href="https://poopoo-api.vercel.app" target="_blank">PooPoo API</b-dropdown-item>
+    <b-dropdown-divider></b-dropdown-divider>
+    <b-dropdown-item href="https://discord.gg/sMXe3nGW6Q" target="_blank">s4d comunity</b-dropdown-item>
+    <b-dropdown-item href="https://discord.gg/fa3Exkkrpc" target="_blank">s4d world</b-dropdown-item>
+    <b-dropdown-item href="https://discord.gg/8Eteb7Pp3j" target="_blank">(WIP) official s4d server</b-dropdown-item>
   </b-nav-item-dropdown>
 </template>
