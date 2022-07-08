23070d31ae93090e93d6f1e2332d66df5cbc3f0f
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-07-08 13:37:25 -0700

    jdsfhjdfsidfufgjfdg my dog is bogernating
    made a second create role that acts as an output

---
+++ src/blocks/godslayerakp/discord/roles/create role then.js
@@ -0,0 +1,44 @@
+import * as Blockly from "blockly/core";
+
+const blockName = "gsa_create_role";
+
+const blockData = {
+    "message0": "%{BKY_CREATE_ROLE}",
+    "args0": [
+        {
+            "type": "input_value",
+            "name": "NAME",
+            "check": [ "String", "Number" ]
+        },
+        {
+            "type": "input_value",
+            "name": "SERVER",
+            "check": [ "Server" ]
+        },
+        {
+            "type":"input_value",
+            "name":"COLOR",
+            "check":"Colour"
+        }
+
+    ],
+    "colour": "#4C97FF",
+    "output": "Role",
+    "inputsInline": false,
+    "tooltip": "",
+    "helpUrl": ""
+};
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function(block){
+    const name = Blockly.JavaScript.valueToCode(block, "NAME", Blockly.JavaScript.ORDER_ATOMIC);
+    const server = Blockly.JavaScript.valueToCode(block, "SERVER", Blockly.JavaScript.ORDER_ATOMIC);
+    const color = Blockly.JavaScript.valueToCode(block, "COLOR", Blockly.JavaScript.ORDER_ATOMIC)
+    const code = `${server}.roles.create({ name: ${name},color:${color} })`;
+    return code;
+};
