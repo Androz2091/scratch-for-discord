dba3bc73e2963cd60741adf91df3dbae8ae658e7
Eruption <94153406+EruptionGuy@users.noreply.github.com>
2022-08-01 11:04:38 +0700

    Create index.js

---
+++ src/blocks/database/index.js
@@ -0,0 +1,12 @@
+import "./create_db";
+import "./get_data";
+import "./has_data";
+import "./get_all_data";
+import "./set_data";
+import "./delete_data";
+import "./add_data";
+import "./subtract_data";
+import "./delete_all";
+
+import "./old_database"
+import "./dootabase"
