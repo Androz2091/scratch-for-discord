65fbe08a5a8d92f16748d1f0c198a0accc27f368
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-07-31 18:39:56 -0700

    AHHHHHHHHH EMBEDS AAAAAAAAAA
    made it so that the embed is message embed only and added the ability to set the message content

--- src/blocks/godslayerakp/discord/embeds/send embed.js
+++ src/blocks/godslayerakp/discord/embeds/send embed.js
@@ -3,14 +3,19 @@ import Blockly from "blockly/core";
 const blockName = "gsa_send_embed";
 const blockData = {
   "type": "gsa_send_embed",
-  "message0": "send embed %1",
+  "message0": "send embed %1 with message %2",
   "args0": [
     {
       "type": "field_input",
       "name": "NAME"
+    },
+    {
+      "type": "input_value",
+      "name": "message",
+      "check": "String"
     }
   ],
-  "output": ['embed', "MessageEmbed", "Embed", "EMBED"],
+  "output": "MessageEmbed",
   "inputsInline": true,
   "colour": 120,
   "tooltip": "can be used to send the embed with the name",
@@ -25,6 +30,10 @@ Blockly.Blocks[blockName] = {
 
 Blockly.JavaScript[blockName] = function (block) {
   // const color = Blockly.JavaScript.valueToCode(block, "title", Blockly.JavaScript.ORDER_ATOMIC)
+  const message = "";
+  if (block.getFieldValue("message") !== null) {
+    message = `content: String(${block.getFieldValue("message")}), `
+  }
   const color = block.getFieldValue("NAME")
-  return [`embeds: [${color.replaceAll(" ", "_").replace(/[!@#\$%\^&\*\(\)-=\]\[\|{}\+`~'":;\?\/\.<>,\/]/g, '_')}]`, Blockly.JavaScript.ORDER_NONE];
+  return [`${message}embeds: [${color.replaceAll(" ", "_").replace(/[!@#\$%\^&\*\(\)-=\]\[\|{}\+`~'":;\?\/\.<>,\/]/g, '_')}]`, Blockly.JavaScript.ORDER_NONE];
 }
