b3f2303d1acb8f99e258b7531c8b70efd30e1d00
Eruption <94153406+EruptionGuy@users.noreply.github.com>
2022-08-27 16:45:58 +0000

    Revamped Navbar! :D

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-gray-1000 p-6">
         <div className="flex items-center flex-shrink-0 text-white mr-6 my-auto">
-            <img src="https://cdn.discordapp.com/attachments/932651845447483428/989379701795594300/unknown.png" alt="scratch" className="fill-current h-10 w-10 mr-2"/>
+            <img src="https://images-ext-1.discordapp.net/external/qT4OReCGZ7tLyu7EOp0ZeM1J755R8yOV5vl_IFKd66E/%3Fsize%3D4096/https/cdn.discordapp.com/icons/932651844344373278/ef5d4de4c598bda58c6edc09e9478b2d.png?width=492&height=492" alt="scratch" className="fill-current h-10 w-10 mr-2"/>
             <span className="font-semibold text-xl tracking-tight">Scratch For Discord</span>
         </div>
         <div className="block lg:hidden">
