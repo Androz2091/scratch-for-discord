3859f03a18b88b12921f13a76f9848b17cdf0933
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-09-13 13:51:08 -0700

    FINNALY
    IT FINNALY WORKS
    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHh

--- src/components/NavigationBar/data.vue
+++ src/components/NavigationBar/data.vue
@@ -1,7 +1,7 @@
 <template>
     <b-nav-item-dropdown text="data" right>
         <b-dropdown-item @click="askForFile">Load data</b-dropdown-item>
-        <input hidden @change="load" id="load-s4dData-code" type="file" accept=".zip"/>
+        <input hidden @change="load" id="load-s4dData-code" type="file" accept=".zip,.data"/>
         <b-dropdown-item @click="dld">Download data</b-dropdown-item>
     </b-nav-item-dropdown>
 </template>
@@ -37,45 +37,31 @@
                         return;
                     }
                     const zip = new JSZip();
-                    console.log(zip)
                     localforage.keys().then(async(data) => {
                         let out = {}
                         for (let i = 0; i < data.length; i++) {
                             out[data[i]] = await localforage.getItem(data[i])
                         }
                         zip.file("localForage.json", JSON.stringify(out));
-                        fetch('https://469-forumstest.jeremygamer13.repl.co/accounts/getUser?username=' + out.FORUMS_USERNAME, {
-                            method: 'GET',
-                            headers: { 
-                                'Content-Type': 'application/json',
-                            },
-                        }).then(result => result.json().then(json => { 
-                            // i have no clue how on earth to make the zip wait for this so your just going to have to deal with this making the forums api throw an error everytime the forum name is null
-                            if (out.FORUMS_USERNAME != null) {
-                                zip.file("forumData.json", JSON.stringify(json));
-                            }
-                            zip.generateAsync({
-                                type: "blob"
-                            })
-                            .then((blob) => {
-                                const a = document.createElement("a");
-                                a.style = "display: none";
-                                document.body.appendChild(a);
-                                const url = window.URL.createObjectURL(blob);
-                                a.href = url;
-                                a.download = 's4d.data';
-                                a.click();
-                                window.URL.revokeObjectURL(url);
-                                document.body.removeChild(a);
-                            });
-                        }))
+                        zip.generateAsync({
+                            type: "blob"
+                        })
+                        .then((blob) => {
+                            const a = document.createElement("a");
+                            a.style = "display: none";
+                            document.body.appendChild(a);
+                            const url = window.URL.createObjectURL(blob);
+                            a.href = url;
+                            a.download = 's4d.data';
+                            a.click();
+                            window.URL.revokeObjectURL(url);
+                            document.body.removeChild(a);
+                        });
                     })
                 });
             },
             load() {
                 const file = document.getElementById("load-s4dData-code").files[0];
-                const documentName = file.name.split(".").slice(0, file.name.split(".").length-1);
-                document.querySelector("#docName").textContent = documentName;
                 const reader = new FileReader();
                 reader.onload = (e) => {
                     JSZip.loadAsync(e.target.result).then((data) => {
@@ -87,7 +73,6 @@
                                     if (err) {
                                         console.warn(err)
                                     }
-                                    console.log(conl[i], content[conl[i]])
                                 })
                             }
                         })
