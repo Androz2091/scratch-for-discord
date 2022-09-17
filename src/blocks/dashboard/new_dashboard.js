98f707f5bffd2fe0d11e348a7bb2b9848be4570a
windows-ubuntu <windows-ubuntu@libero.it>
2022-09-17 21:35:24 +0200

    Improved "create new dashboard"

--- src/blocks/dashboard/new_dashboard.js
+++ src/blocks/dashboard/new_dashboard.js
@@ -4,7 +4,7 @@ const blockName = "initialize_dashboard";
 
 const blockData = {
     "type": "initialize_dashboard",
-    "message0": "Create a new dashboard with %1 Name %2 Description %3 Support server URL %4 Bot Invite URL %5 Dashboard URL %6 Port %7 No Port In CallBack URL %8 Client Secret %9 Log Requests %10 Inject CSS %11 Icon Path %12 Theme %13 %14 Permissions required to access the dashboard %15",
+    "message0": "Create a new dashboard with %1 Name %2 Description %3 Support server URL %4 Bot Invite URL %5 Dashboard URL %6 Port %7 No Port In CallBack URL %8 Client Secret %9 Log Requests %10 Inject CSS %11 Icon Path %12 Theme %13 %14 %15 Permissions required to access the dashboard %16",
     "args0": [
         {
             "type": "input_dummy"
@@ -47,10 +47,7 @@ const blockData = {
         {
             "type": "input_value",
             "name": "secret",
-            "check": [
-                "String",
-                "frost_env"
-            ]
+            "check": "String"
         },
         {
             "type": "input_value",
@@ -77,8 +74,17 @@ const blockData = {
                 [
                     "Dark",
                     "dark"
+                ],
+                [
+                    "Custom...",
+                    "custom"
                 ]
             ]
+        },
+        {
+            "type": "field_input",
+            "name": "custom_theme",
+            "text": "Custom theme (used only if selected \"custom\")"
         },
         {
             "type": "input_dummy"
@@ -102,6 +108,7 @@ Blockly.Blocks[blockName] = {
     }
 };
 
+
 Blockly.JavaScript['initialize_dashboard'] = function (block) {
     var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
     var value_description = Blockly.JavaScript.valueToCode(block, 'description', Blockly.JavaScript.ORDER_ATOMIC);
@@ -115,9 +122,32 @@ Blockly.JavaScript['initialize_dashboard'] = function (block) {
     var value_injectcss = Blockly.JavaScript.valueToCode(block, 'injectCSS', Blockly.JavaScript.ORDER_ATOMIC);
     var value_favicon_path = Blockly.JavaScript.valueToCode(block, 'favicon_path', Blockly.JavaScript.ORDER_ATOMIC);
     var dropdown_theme = block.getFieldValue('theme');
+    var text_custom_theme = block.getFieldValue('custom_theme');
     var value_permissions = Blockly.JavaScript.valueToCode(block, 'permissions', Blockly.JavaScript.ORDER_ATOMIC);
+    var code;
     // TODO: Assemble JavaScript into code variable.
-    var code = `
+    if (dropdown_theme == 'custom') {
+
+        code = `
+    s4d.client.dashboard = new Dashboard(s4d.client, {
+        name: ${value_name},
+        description: ${value_description},
+        serverUrl: ${value_support_server_url},
+        inviteUrl: ${value_bot_invite_url},
+        baseUrl: ${value_base_url},
+        port: ${value_port},
+        noPortIncallbackUrl: ${value_noportincallbackurl},
+        secret: ${value_secret},
+        logRequests: ${value_logrequests},
+        injectCSS: ${value_injectcss},
+        faviconPath: ${value_favicon_path},
+        theme: ${text_custom_theme},
+        permissions: ${value_permissions},
+    });
+`;
+    }
+    else {
+        code = `
     s4d.client.dashboard = new Dashboard(s4d.client, {
         name: ${value_name},
         description: ${value_description},
@@ -134,5 +164,6 @@ Blockly.JavaScript['initialize_dashboard'] = function (block) {
         permissions: ${value_permissions},
     });
 `;
+    }
     return code;
 };
