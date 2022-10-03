1b753e20e09846107817e46f826e4f0f41b66064
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-10-03 16:30:19 -0700

    added roles list

--- src/toolbox.js
+++ src/toolbox.js
@@ -2655,6 +2655,7 @@ export default (Blockly, value, searching) => {
             <block type="s4d_get_all_role_role"></block>
             <block type="jg_roles_get_all_member_roles_then_for_each_do"/>
             <block type="jg_roles_get_all_member_roles_then_for_each_do_role"/>
+            <block type="gsa_get_all_roles_on_member"></block>
             <block type="hig_role"/>
             <!--
             <block type="monaco_members_with_role"/>
