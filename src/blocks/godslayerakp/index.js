090a57d6e93a19469dce8a6b1482d7ff00ceca09
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-10-12 23:00:56 -0700

    async async :trollface:

--- src/blocks/godslayerakp/index.js
+++ src/blocks/godslayerakp/index.js
@@ -56,30 +56,65 @@ Blockly.JavaScript["gsa_bypass_type"] = function (block) {
 }
 
 Blockly.Blocks["gsa_format_time"] = {
-    init: function () {
-      this.jsonInit({
-        "message0": "make time stamp from %1 with format %2",
-        "args0": [
-          {
-            "type": "input_value",
-            "name": "time",
-            "check": "Number"
-          },
-          {
-              "type": "input_value",
-              "name": "format",
-              "check": "String"
-          }
-        ],
-        "output": "String",
-        "inputsInline": true,
-        "colour": '#D14081',
-        "tooltip": "formats date as timestamp (like discords timestamps but with more configurable)",
-        "helpUrl": "https://momentjscom.readthedocs.io/en/latest/moment/04-displaying/01-format/"
-      });
-    }
-  };
-  
-  Blockly.JavaScript["gsa_format_time"] = function (block) {
-    return [`String(moment(${Blockly.JavaScript.valueToCode(block, "time", Blockly.JavaScript.ORDER_ATOMIC)}).format(${Blockly.JavaScript.valueToCode(block, "format", Blockly.JavaScript.ORDER_ATOMIC)}))`, Blockly.JavaScript.ORDER_ATOMIC];
+  init: function () {
+    this.jsonInit({
+      "message0": "make time stamp from %1 with format %2",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "time",
+          "check": "Number"
+        },
+        {
+          "type": "input_value",
+          "name": "format",
+          "check": "String"
+        }
+      ],
+      "output": "String",
+      "inputsInline": true,
+      "colour": '#D14081',
+      "tooltip": "formats date as timestamp (like discords timestamps but with more configurable)",
+      "helpUrl": "https://momentjscom.readthedocs.io/en/latest/moment/04-displaying/01-format/"
+    });
   }
+};
+
+Blockly.JavaScript["gsa_format_time"] = function (block) {
+  return [`String(moment(${Blockly.JavaScript.valueToCode(block, "time", Blockly.JavaScript.ORDER_ATOMIC)}).format(${Blockly.JavaScript.valueToCode(block, "format", Blockly.JavaScript.ORDER_ATOMIC)}))`, Blockly.JavaScript.ORDER_ATOMIC];
+}
+
+
+
+Blockly.Blocks["gsa_async_async_troll"] = {
+  init: function () {
+    this.jsonInit({
+      "type": "block_type",
+      "message0": "async %1 %2",
+      "args0": [
+        {
+          "type": "input_dummy"
+        },
+        {
+          "type": "input_statement",
+          "name": "code"
+        }
+      ],
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": 230,
+      "tooltip": "Asynchronously runs the code inside of it",
+      "helpUrl": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function"
+    });
+  }
+};
+
+Blockly.JavaScript['gsa_async_async_troll'] = function(block) {
+  var statements_code = Blockly.JavaScript.statementToCode(block, 'code');
+  var code = `
+(async () => {
+  ${statements_code}
+})
+`;
+  return code;
+};
