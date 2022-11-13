1ca1aef37337bb3f320df75d87fdb7a407fe8ba0
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-11-12 16:23:21 -0800

    better when member clicks button block (jose)

--- src/blocks/discord/discord-button/create-button-collector.js
+++ src/blocks/discord/discord-button/create-button-collector.js
@@ -3,7 +3,7 @@ import * as Blockly from "blockly";
 const blockName = "s4d_on_click";
 
 const blockData = {
-    "message0": "%{BKY_ON_CLICK}",
+    "message0": "when a button is clicked by member %3 before %4 (in seconds, leave blank for default) %1 %2",
     "colour": "#F5AB1A",
     "args0": [
         {
@@ -17,6 +17,11 @@ const blockData = {
             "type": "input_value",
             "name": "MEMBER",
             "check": "Member"
+        },
+        {
+            "type": "input_value",
+            "name": "time",
+            "check": "Number"
         }
     ],
     "previousStatement": null,
@@ -33,12 +38,18 @@ Blockly.Blocks[blockName] = {
 Blockly.JavaScript[blockName] = function (block) {
     const statements = Blockly.JavaScript.statementToCode(block, "STATEMENTS");
     const memberr = Blockly.JavaScript.valueToCode(block, "MEMBER", Blockly.JavaScript.ORDER_ATOMIC);
+    const time = Blockly.JavaScript.valueToCode(block, "time", Blockly.JavaScript.ORDER_ATOMIC);
     let member = memberr.replace(".user", "")
     if (String(member) == null || String(member) == "") member = "i.user"
-    const code = `let collector = m.createMessageComponentCollector({filter: i=>i.user.id === ${member}.id ,time:60000});
-    collector.on('collect',async i=>{
-        ${statements}
-    })\n`;
+    const code = `
+let collector = m.createMessageComponentCollector({
+    filter: i => i.user.id === ${member}.id,
+    time: Number(${time < 1 ? '60000' : time}) * 1000
+});
+collector.on('collect', async i => {
+    ${statements}
+})
+`;
     return code;
 };
 
